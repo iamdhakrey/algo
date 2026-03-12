dd56dabc66da5236d204b2c8a12eb3cd814685a2
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2026-03-12 15:50:56 +0530

    GO: Add Factorial Code

--- loop.go
+++ loop.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func factorial(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
 func main() {
 	// fmt.Println("Test")
 	var test_list []string
@@ -9,28 +16,28 @@ func main() {
 		"heelp", "gesg",
 	}
 	fmt.Println(test_list)
-	for li,fsd := range test_list{
-		fmt.Println(li,fsd)
+	for li, fsd := range test_list {
+		fmt.Println(li, fsd)
 	}
 
 	var test2 map[string]string
-	test2 = map[string]string {
-		"eter":"tret",
+	test2 = map[string]string{
+		"eter": "tret",
 		"tret": "trete",
 	}
 	fmt.Println(test2)
-	for li,fsd := range test2{
-		fmt.Println(li,fsd)
+	for li, fsd := range test2 {
+		fmt.Println(li, fsd)
 	}
 
-	z :=20
-	for i := 0; i<z; i+=10 {
+	z := 20
+	for i := 0; i < z; i += 10 {
 		fmt.Println(i)
 	}
 
 	for i := 5; i > 0; i-- {
 
-		for j :=1; j >=i; j-- {
+		for j := 1; j >= i; j-- {
 			if i == j {
 				fmt.Println("*")
 
@@ -39,4 +46,8 @@ func main() {
 			}
 		}
 	}
+
+	fec := factorial(5)
+	fmt.Println("gfe")
+	fmt.Println(fec)
 }
